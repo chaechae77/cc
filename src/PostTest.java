580555c975b0b5e5a27f6d225913db0d3f87171f
chaechae77 <catross77@naver.com>
2023-01-25 16:40:13 +0900

    주석추가

--- src/PostTest.java
+++ src/PostTest.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PostTest {
+public class PostTest { // d안녕
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int id = sc.nextInt();
